97ae404c3d104d4b75e5f75d15caa6b640957f4c
HASHTAG <nawwjh@gmail.com>
2024-07-01 20:04:23 +0800

    .gitignore WebTuts11/__pycache__/ WebTuts11/migrations/__pycache__/ WebTuts11/templates/ WebTuts11/urls.py requirements.txt

--- WebTut11/settings.py
+++ WebTut11/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'WebTuts11',
+    'bootstrap5',
+
 ]
 
 MIDDLEWARE = [
--- WebTuts11/views.py
+++ WebTuts11/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index (request):
+    return HttpResponse("Hello, world. You're at the main page index.")
+
+def camera_page (request):
+    return render(request,'WebTuts11/camera.html',{})
+
+def audio_page (request):
+    return render(request,'WebTuts11/audio.html',{})
