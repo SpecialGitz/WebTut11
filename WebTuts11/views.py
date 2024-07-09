997cfae831d15727b7913ac6b9a17fc0b3f5fe2d
Isaiah-Ang <isaiah.ang@gmail.com>
2024-07-09 12:46:54 +0800

    upload image file

--- WebTuts11/views.py
+++ WebTuts11/views.py
@@ -1,5 +1,11 @@
+import os
+
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from WebTut11 import settings
+from .forms import ImageUploadForm
 
 
 # Create your views here.
@@ -7,7 +13,26 @@ def index (request):
     return HttpResponse("Hello, world. You're at the main page index.")
 
 def camera_page (request):
-    return render(request,'WebTuts11/camera.html',{})
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.cleaned_data.get("name")
+            img = request.FILES["image"]
+
+            _, file_extension = os.path.splitext(img.name)
+
+            upload_dir = os.path.join(settings.BASE_DIR, 'static', 'Upload')
+
+        file_path = os.path.join(upload_dir, name + file_extension)
+        with open(file_path, 'wb+') as destination:
+            for chunk in img.chunks():
+                destination.write(chunk)
+
+    else:
+        form = ImageUploadForm()
+
+    context = {'form': form}
+    return render(request,'WebTuts11/camera.html', context)
 
 def audio_page (request):
     return render(request,'WebTuts11/audio.html',{})
